a84d4d8f3b2b29cbd19be3b0259954dd78975fbf
rishabh-infobeans <rishabh.dwivedi@infobeans.com>
2016-10-10 19:27:11 +0530

    with test cases

--- app/code/local/Buyshop/Faq/controllers/IndexController.php
+++ app/code/local/Buyshop/Faq/controllers/IndexController.php
@@ -16,18 +16,6 @@ class Buyshop_Faq_IndexController extends Mage_Core_Controller_Front_Action {
     public function IndexAction() {
         $this->loadLayout();
         $this->getLayout()->getBlock("head")->setTitle($this->__("FAQ"));
-//        $breadcrumbs = $this->getLayout()->getBlock("breadcrumbs");
-//        $breadcrumbs->addCrumb("home", array(
-//            "label" => $this->__("Home"),
-//            "title" => $this->__("Home"),
-//            "link" => Mage::getBaseUrl()
-//        ));
-//
-//        $breadcrumbs->addCrumb("faq", array(
-//            "label" => $this->__("Faq"),
-//            "title" => $this->__("Faq")
-//        ));
-
         $this->renderLayout();
     }
 
---
+++ tests/unit/testsuite/Buyshop/Faq/Block/FaqTest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class Buyshop_Faq_Block_FaqTest extends PHPUnit_Framework_TestCase {
+    protected $datasets;
+    public function setUp() {
+        Mage::app('en');
+    }
+    
+    public function testConstruct(){
+       $datasets=Mage::getModel('faq/faq')->getCollection();
+        $this->assertInstanceOf('Buyshop_Faq_Model_Mysql4_Faq_Collection',$datasets);
+    }
+    public function testDataset(){
+       $datasets=Mage::getModel('faq/faq')->getCollection()->getSize();
+       $this->assertGreaterThanOrEqual(0,$datasets);
+    }
+}
